b93ee85c65f6d83a9145e6c3764fa57b937f451c
Leandro Collares <leco@uvic.ca>
2019-07-01 09:14:09 -0300

    Updates

---
+++ precache-manifest.2f1d0e8e744f7537e03931b4453b7b49.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f35589fe14edfc7db73fba44f0dbfb21",
+    "url": "/with-or-without-curry/index.html"
+  },
+  {
+    "revision": "243200a6eb23c9318ce9",
+    "url": "/with-or-without-curry/static/css/main.6664e91b.chunk.css"
+  },
+  {
+    "revision": "d4d82622109e39d2c0b7",
+    "url": "/with-or-without-curry/static/js/2.f159276b.chunk.js"
+  },
+  {
+    "revision": "243200a6eb23c9318ce9",
+    "url": "/with-or-without-curry/static/js/main.964b3b67.chunk.js"
+  },
+  {
+    "revision": "0f86f33b800f5baefe85",
+    "url": "/with-or-without-curry/static/js/runtime~main.8442b8da.js"
+  },
+  {
+    "revision": "1efbd38aa76ddae2580fedf378276333",
+    "url": "/with-or-without-curry/static/media/lato-v15-latin-700.1efbd38a.woff2"
+  },
+  {
+    "revision": "874b8e7bc7e8d1507b50f56bc6c9b536",
+    "url": "/with-or-without-curry/static/media/lato-v15-latin-700.874b8e7b.woff"
+  },
+  {
+    "revision": "fe7d53e4cac8f5b946a9e25f004efc8c",
+    "url": "/with-or-without-curry/static/media/lato-v15-latin-700.fe7d53e4.ttf"
+  },
+  {
+    "revision": "b4d2c4c39853ee244272c04999b230ba",
+    "url": "/with-or-without-curry/static/media/lato-v15-latin-regular.b4d2c4c3.woff2"
+  },
+  {
+    "revision": "b8ee546acd6cc0c49f42ad3d48ef244f",
+    "url": "/with-or-without-curry/static/media/lato-v15-latin-regular.b8ee546a.woff"
+  },
+  {
+    "revision": "d1d5e30aa8bb081cad8f409f27861672",
+    "url": "/with-or-without-curry/static/media/lato-v15-latin-regular.d1d5e30a.ttf"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/with-or-without-curry/precache-manifest.2f1d0e8e744f7537e03931b4453b7b49.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/with-or-without-curry/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
